91f11db762ebc0353b5c813979e26f55f6dacf6e
Ayden Woo <ztwu@bu.edu>
2021-08-15 11:16:00 -0700

    population reader

--- src/edu/upenn/cit594/datamanagement/PopulationReader.java
+++ src/edu/upenn/cit594/datamanagement/PopulationReader.java
@@ -1,4 +1,42 @@
 package edu.upenn.cit594.datamanagement;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+
 public class PopulationReader {
+    protected String filename;
+
+    public PopulationReader(String filename){
+        this.filename = filename;
+    }
+
+    public HashMap<Integer, Integer> getPopulation() {
+        // <key, value> -> <zip, population>
+        HashMap<Integer, Integer> populationMap = new HashMap<Integer, Integer>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String [] lineTemp = line.split("\\s+");
+                Integer zipcode = Integer.parseInt(lineTemp[0].strip());
+                Integer population = Integer.parseInt(lineTemp[1].strip());
+                populationMap.put(zipcode, population);
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error: population file is not accessible");
+            System.exit(0);
+        }
+        return populationMap;
+    }
+
+//    public static void main(String[] args){
+//        PopulationReader test = new PopulationReader("/Users/zetianwu/Desktop/UPENN:MCIT/594/Group Project/CIT594-Project/population.txt");
+//        HashMap<Integer, Integer> result = test.getPopulation();
+//        for (Integer zip : result.keySet()) {
+//            System.out.println(zip);
+//        }
+//    }
 }
--- src/edu/upenn/cit594/util/Property.java
+++ src/edu/upenn/cit594/util/Property.java
@@ -1,4 +1,26 @@
 package edu.upenn.cit594.util;
 
+import java.util.Date;
+
 public class Property {
+
+    private int zipcode;
+    private int marketValue;
+    private String buildingCode;
+    private int totalLivableArea;
+
+
+
+    public Property(int zipcode, int marketValue, String buildingCode, int totalLivableArea) {
+        this.zipcode = zipcode;
+        this.buildingCode = buildingCode;
+        this.marketValue = marketValue;
+        this.totalLivableArea = totalLivableArea;
+    }
+
+
+    public int getZipcode() {return zipcode;}
+    public int getTotalLivableArea() {return totalLivableArea;}
+    public int getMarketValue() {return marketValue;}
+    public String getBuildingCode() {return buildingCode;}
 }
